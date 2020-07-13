e999b823d6c8604429bb5f523d761ab19e99db29
gwiedeman <gregory.wiedeman1@gmail.com>
2020-07-13 17:12:09 -0400

    now serves in subpath

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :citations
-  resources :subjects
-  resources :events
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  devise_scope :user do
-  	root to: "events#index"
+  scope 'history-editor' do
+      devise_for :users
+      resources :citations
+      resources :subjects
+      resources :events
+      # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      devise_scope :user do
+        root to: "events#index"
+      end
   end
 end
