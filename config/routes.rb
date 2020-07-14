41fdc0c58fea8f9e7d0cb6edecf35558bc30b91d
gwiedeman <gregory.wiedeman1@gmail.com>
2020-07-14 11:42:36 -0400

    hopefully fixes devise redirects in production

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
       resources :subjects
       resources :events
       # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-      devise_scope :user do
-        root to: "events#index"
-      end
+  end
+  devise_scope :user do
+    root to: "events#index"
   end
 end
