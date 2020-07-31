592bcaa364d9626b58990c9e6811e6fc93fda10a
gwiedeman <gregory.wiedeman1@gmail.com>
2020-07-31 15:10:16 -0400

    removed citations link uniqueness validator so you can link the same object to multiple places

--- app/models/citation.rb
+++ app/models/citation.rb
@@ -2,7 +2,6 @@ class Citation < ApplicationRecord
 	belongs_to :events, optional: true
 
 	validates :link, presence: true
-	validates :link, uniqueness: { case_sensitive: false }
 	validate :link_has_correct_format
 
 	def link_has_correct_format
