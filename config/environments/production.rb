17147ab6bff2e93a1c3d4297492a07ae9fd11601
Greg Wiedeman <gregory.wiedeman1@gmail.com>
2022-12-14 11:14:00 -0500

    setup email host for prod devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "history-editor_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'archives.albany.edu' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
