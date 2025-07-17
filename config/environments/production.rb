726929b0e7a7a8caf77b6fad82b57a822a169ba2
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-07-17 15:16:37 -0400

    whent back to old settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
   config.assets.prefix = '/history-editor/assets'
-  config.relative_url_root = '/history-editor'
+  #config.relative_url_root = '/history-editor'
   config.public_file_server.enabled = true
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
