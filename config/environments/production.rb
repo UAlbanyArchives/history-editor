de9c8e0cb654c08ffec45b020d5392cd0a191dc4
Gregory Wiedeman <gregory.wiedeman1@gmail.com>
2025-07-17 14:34:14 -0400

    assets should serve here

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.prefix = '/history-editor'
+  config.assets.prefix = '/history-editor/assets'
   #config.relative_url_root = '/history-editor'
   config.public_file_server.enabled = true
   
